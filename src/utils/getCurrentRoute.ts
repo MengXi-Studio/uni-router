819c9450ac40466d3dde3e8ab34073d69598e253
queyupeng <ksmengfang@163.com>
2025-07-24 12:24:05 +0800

    优化项目配置

--- src/utils/getCurrentRoute.ts
+++ src/utils/getCurrentRoute.ts
@@ -14,27 +14,29 @@ export function getCurrentRoute(currentPage: CurrentPage | null): Route | null {
 
 	// 使用条件编译处理多平台差异，初始化查询参数对象，默认为空对象
 	let options: Record<string, string> = {}
+	// 获取当前的平台标识
+	const key = String(process.env.UNI_PLATFORM).toString().toLowerCase()
 
 	// 在微信小程序、支付宝小程序、百度小程序、头条小程序、QQ 小程序平台下，获取页面参数
 	// 这里表示在指定的小程序平台下，从当前页面实例的 options 属性获取查询参数
 	// 若 options 不存在，则使用空对象
-	// #ifdef MP-WEIXIN || MP-ALIPAY || MP-BAIDU || MP-TOUTIAO || MP-QQ
-	options = currentPage.options || {}
-	// #endif
+	if (['mp-weixin', 'mp-alipay', 'mp-baidu', 'mp-toutiao', 'mp-qq'].includes(key)) {
+		options = currentPage.options || {}
+	}
 
 	// 在 H5 平台下，从 Vue 实例的路由信息中获取查询参数
 	// 通过 $vm 访问 Vue 实例，再从 $route 属性中获取查询参数
 	// 若相关属性不存在，则使用空对象
-	// #ifdef H5
-	options = currentPage.$vm?.$route?.query || {}
-	// #endif
+	if (['h5'].includes(key)) {
+		options = currentPage.$vm?.$route?.query || {}
+	}
 
 	// 在 App 平台下，从 Vue 实例的小程序相关信息中获取查询参数
 	// 通过 $vm 访问 Vue 实例，再从 $mp.query 属性中获取查询参数
 	// 若相关属性不存在，则使用空对象
-	// #ifdef APP-PLUS
-	options = currentPage.$vm?.$mp?.query || {}
-	// #endif
+	if (['app-plus'].includes(key)) {
+		options = currentPage.$vm?.$mp?.query || {}
+	}
 
 	// 返回当前路由对象
 	return {
